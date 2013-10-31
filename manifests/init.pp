be2736d74f986428f281edb24c8ab28da7c78467
root <root@master.(none)>
2013-10-31 22:21:34 +0000

    Added template files, updated manifest

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,18 +3,24 @@ class nginx_conf {
   #add official nginx repo
   file {"create nginx repo":
     ensure	=> 	file,
-    source	=> 	"puppet:///nginx.repo",
+    path	=>	"/etc/yum.repos.d/nginx.repo",
+    source	=> 	"puppet:///modules/nginx_conf/nginx.repo",
   } 
+
 
   #install nginx package
   package {"install nginx":
     ensure 	=> 	latest,
     name	=>	"nginx",
+    require	=>	File["create nginx repo"],
   }
 
+
   #update nginx.conf file to host over port 8081, and serve custom web content
-  file {"nginx.conf":
-    
+  file {"nginx default config":
+    ensure	=>	file,
+    path	=>	"/etc/nginx/conf.d/default.conf",
+    source	=>	"puppet:///modules/nginx_conf/default.conf",
   }
 
 
@@ -30,7 +36,7 @@ class nginx_conf {
   user {"nginx-user":
     name	=>	"nginx-user",
     uid		=>	777,
-    group	=>	777,
+    gid		=>	777,
     shell	=>	"/bin/bash",
     home	=>	"/home/nginx-user",
   }
@@ -39,6 +45,6 @@ class nginx_conf {
   #restart the service when the nginx.conf file changes
   service {"nginx":
     ensure 	=>	running,
-    subscribe	=>	File["nginx.conf"],
+    subscribe	=>	File["nginx default config"],
   }
 }
