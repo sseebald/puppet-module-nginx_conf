28e0fc289e6e314dc0d6d1513444b35d24c339b7
root <root@master.(none)>
2013-11-01 03:01:38 +0000

    Added install and uninstall functionality

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,50 +1,76 @@
-class nginx_conf {
+class nginx_conf(
+  $action = "install",) {
 
-  #add official nginx repo
-  file {"create nginx repo":
-    ensure	=> 	file,
-    path	=>	"/etc/yum.repos.d/nginx.repo",
-    source	=> 	"puppet:///modules/nginx_conf/nginx.repo",
-  } 
+  if $action == 'install'{
+
+    #add official nginx repo
+    file {"create nginx repo":
+      ensure	=> 	file,
+      path	=>	"/etc/yum.repos.d/nginx.repo",
+      source	=> 	"puppet:///modules/nginx_conf/nginx.repo",
+    } 
 
 
-  #install nginx package
-  package {"install nginx":
-    ensure 	=> 	latest,
-    name	=>	"nginx",
-    require	=>	File["create nginx repo"],
+    #install nginx package
+    package {"install nginx":
+      ensure 	=> 	latest,
+      name	=>	"nginx",
+      require	=>	File["create nginx repo"],
+    }
+
+
+    #update nginx.conf file to host over port 8081, and serve custom web content
+    file {"nginx default config":
+      ensure	=>	file,
+      path	=>	"/etc/nginx/conf.d/default.conf",
+      source	=>	"puppet:///modules/nginx_conf/default.conf",
+    }
+
+
+    #create new group for nginx user
+    group {"nginx-user group add":
+      ensure	=>	present,
+      name	=>	"nginx-user",
+      gid		=>	777,
+    }
+
+
+    #create new nginx user
+    user {"nginx-user add":
+      ensure	=>	present,
+      name	=>	"nginx-user",
+      uid		=>	777,
+      gid		=>	777,
+      shell	=>	"/bin/bash",
+      home	=>	"/home/nginx-user",
+    } 
+
+
+    #restart the service when the nginx.conf file changes
+    service {"nginx":
+      ensure 	=>	running,
+      subscribe	=>	File["nginx default config"],   
+    }
   }
+  else {
 
+    #remove nginx user
+    user {"nginx-user remove":
+      ensure	=>	absent,
+      name	=>	"nginx-user",
+    } 
 
-  #update nginx.conf file to host over port 8081, and serve custom web content
-  file {"nginx default config":
-    ensure	=>	file,
-    path	=>	"/etc/nginx/conf.d/default.conf",
-    source	=>	"puppet:///modules/nginx_conf/default.conf",
-  }
+    #remove group for nginx user
+    group {"nginx-user group remove":
+      ensure	=>	absent,
+      name	=>	"nginx-user",
+    }
 
-
-  #create new group for nginx user
-  group {"nginx-user":
-    ensure	=>	present,
-    name	=>	"nginx-user",
-    gid		=>	777,
-  }
-
-
-  #create new nginx user
-  user {"nginx-user":
-    name	=>	"nginx-user",
-    uid		=>	777,
-    gid		=>	777,
-    shell	=>	"/bin/bash",
-    home	=>	"/home/nginx-user",
-  }
-
-
-  #restart the service when the nginx.conf file changes
-  service {"nginx":
-    ensure 	=>	running,
-    subscribe	=>	File["nginx default config"],
+    #remove nginx package
+    package {"remove nginx":
+      ensure 	=> 	absent,
+      name	=>	"nginx",
+    }
   }
 }
+
